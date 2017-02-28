e64727e150209e1a1d88458872567e43c2700dba
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-02-28 17:16:08 +0100

    Refactor components to use namespaced functions

--- child/Http/assets.php
+++ child/Http/assets.php
@@ -2,8 +2,26 @@
 
 namespace {{ theme.namespace }}\Theme\Http;
 
+/*
+|-----------------------------------------------------------------
+| Child Theme Assets
+|-----------------------------------------------------------------
+|
+| This file is for registering your child theme
+| stylesheets and scripts, which will be load
+| additionaly to the parent theme assets.
+|
+|
+*/
+
 use function {{ theme.namespace }}\Theme\asset_path;
 
-add_action('wp_enqueue_scripts', function () {
-    // wp_enqueue_style('child-theme', asset_path('css/child-theme.css'));
-});
+/**
+ * Registers child theme stylesheet files.
+ *
+ * @return void
+ */
+function register_stylesheets() {
+    wp_enqueue_style('child-theme-stylesheet', asset_path('css/child-theme.css'));
+}
+add_action('wp_enqueue_scripts', '{{ theme.namespace }}\Http\register_stylesheets');
